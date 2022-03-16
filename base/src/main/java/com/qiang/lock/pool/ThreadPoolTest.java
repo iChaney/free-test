78e7bd322add5d8d289fd7aa98d23be2366b5939
xutingting <xutingting@efort.com.cn>
2022-03-16 23:34:22 +0800

    初始化

--- base/src/main/java/com/qiang/lock/pool/ThreadPoolTest.java
+++ base/src/main/java/com/qiang/lock/pool/ThreadPoolTest.java
@@ -5,34 +5,26 @@ import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * @author liq
  * @date 2021/6/22 18:24
  */
 public class ThreadPoolTest {
-    private static AtomicInteger count = new AtomicInteger(0);
 
     public static void main(String[] args) {
         ThreadPoolExecutor tpe =
-                new ThreadPoolExecutor(4, 10, 3000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(200), new ThreadFactory() {
-                    @Override
-                    public Thread newThread(Runnable r) {
-                        Thread thread = new Thread(r);
-                        thread.setName("my-thread");
-                        return thread;
-                    }
-                });
-        for (int i = 0; i < 214; i++) {
+                new ThreadPoolExecutor(4, 210, 3000, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<Runnable>(200), new MyThreadFactory());
+        for (int i = 0; i < 100; i++) {
             tpe.execute(new SimpleRunable());
             System.out.printf("queue数量:%s%n", tpe.getQueue().size());
         }
         try {
-            Thread.sleep(5000);
+            Thread.sleep(30000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.printf("一共运行了%s次任务", count);
     }
 
     static class SimpleRunable implements Runnable {
@@ -44,7 +36,16 @@ public class ThreadPoolTest {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            count.getAndIncrement();
+        }
+    }
+
+    static class MyThreadFactory implements ThreadFactory {
+        private final AtomicLong threadIndex = new AtomicLong(0);
+
+        @Override
+        public Thread newThread(Runnable r) {
+            Thread thread = new Thread(r, "my-t2hread" + threadIndex.incrementAndGet());
+            return thread;
         }
     }
 }
